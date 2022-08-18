83bd8d63ff6fd2d00d40c2cdb1e60d60e30d5f93
Filippa <filippabellange@live.se>
2022-08-18 10:48:19 +0200

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Layout } from "./components/Layout";
+import { NotFound } from "./components/NotFound";
+import { Admin } from "./components/pages/Admin";
+import { Home } from "./components/pages/Home";
 
 function App() {
-	const [thing, setThing] = useState<any>({newCustomer: {name: "test"}})
-
-	useEffect(() => {
-		axios.get("http://localhost:8000").then(response => {
-			setThing(response.data)
-			console.log(response.data)
-		})
-	}, [])
-
-	return (
-		<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>Edit <code>src/App.tsx</code> and save to reload.</p>
-			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-		</header>
-		{thing.newCustomer.name}
-		</div>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/admin" element={<Admin />}></Route>
+          </Route>
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
