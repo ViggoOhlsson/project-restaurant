353291f820c7b4aaaa6da8556b94731d2555b984
ViggoOhlsson <viggo.e.g.o@gmail.com>
2022-08-18 10:35:53 +0200

    tog bort skräpkod

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 
 function App() {
-	const [thing, setThing] = useState<any>({newCustomer: {name: "test"}})
-
-	useEffect(() => {
-		axios.get("http://localhost:8000").then(response => {
-			setThing(response.data)
-			console.log(response.data)
-		})
-	}, [])
-
 	return (
-		<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>Edit <code>src/App.tsx</code> and save to reload.</p>
-			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-		</header>
-			{thing.newCustomer.name}
-		</div>
+		<div>Hello World!</div>
 	);
 }
 
