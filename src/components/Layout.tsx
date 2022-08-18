bd372538ce82b7ec3e9ad1413d2942ef1546369a
ViggoOhlsson <viggo.e.g.o@gmail.com>
2022-08-18 11:23:58 +0200

    la till nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
     <>
+      <nav>
+        <Link to="/">Home</Link> 
+        <Link to="/book">Create Booking</Link> 
+        <Link to="/admin">Admin</Link> 
+        <Link to="/test">404 test</Link> 
+      </nav>
       <Outlet></Outlet>
     </>
   );
--- src/components/pages/Booking.tsx
+++ src/components/pages/Booking.tsx
@@ -1,6 +1,9 @@
 export function Booking() {
-    
+
     return <>
         <p>Booking View</p>
+        <form>
+            Här har vi en form för grejor och saker liksom
+        </form>
     </>
 }
