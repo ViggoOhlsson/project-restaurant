5393ebc170d0b9d87d5f65d676ea5d50385bc254
ViggoOhlsson <viggo.e.g.o.@gmail.com>
2022-08-19 11:10:59 +0200

    dsa

--- src/components/pages/Booking.tsx
+++ src/components/pages/Booking.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
+
 export function Booking() {
+
+    const [customerName, setCustomerName] = useState("")
 
     return <>
         <p>Booking View</p>
         <form>
+            <input type="text" onInput={setCustomerName(e)}></input>
+
             Här har vi en form för grejor och saker liksom
         </form>
     </>
