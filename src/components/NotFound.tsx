2d71bbe225dbcaae970096e0daca49f35b74977c
johannafryxell <johanna.fryxell@gmail.com>
2022-08-22 11:26:36 +0200

    JSX structure done

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,3 +1,15 @@
 export function NotFound() {
-  return <>404</>;
+  return (
+    <main className="error-page">
+      <h2 className="error-page__section error-page__section--heading">404</h2>
+
+      <div className="error-page__section error-page__section--info">
+        <span className="info__text">Page not found</span>
+
+        <a className="info__btn" href="/">
+          Back to home page
+        </a>
+      </div>
+    </main>
+  );
 }
