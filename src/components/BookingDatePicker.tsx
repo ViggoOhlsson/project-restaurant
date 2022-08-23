e5a531b08c67c9cd1eda20280a4c45398f8ed0ef
ViggoOhlsson <viggo.e.g.o@gmail.com>
2022-08-23 11:55:04 +0200

    dsa

---
+++ src/components/BookingDatePicker.tsx
@@ -0,0 +1,5 @@
+export const BookingDatePicker = () => {
+    return <div className="booking-date-picker">
+        
+    </div>
+}
--- src/components/BookingPhase.tsx
+++ src/components/BookingPhase.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react"
 import "../scss/main.scss"
 
 interface IBookingPhaseProps {
     phase: number
 }
 
-export const BookingPhase = () => {
+export const BookingPhase = (props: IBookingPhaseProps) => {
 
+    const [phase, setPhase] = useState(props.phase)
 
+    console.log(phase)
 
     return <div className="booking-phase-container">
-        <div className="booking-phase phase-date">
+    <button onClick={() => {setPhase(phase - 1)}}>- 1</button>
+    <button onClick={() => {setPhase(phase + 1)}}>+ 1</button>
+        <div className={`booking-phase phase-date ${phase == 1 && 'current'}`}>
             <span className="phase-number">
                 1
             </span>
@@ -17,20 +22,31 @@ export const BookingPhase = () => {
                 Date
             </span>
         </div>
-        <div className="booking-phase phase-date">
+        <div className="dot"></div>
+        <div className={`booking-phase phase-date ${phase == 2 && 'current'}`}>
             <span className="phase-number">
-                1
+                2
             </span>
             <span className="phase-name">
-                Date
+                Information
             </span>
         </div>
-        <div className="booking-phase phase-date">
+        <div className="dot"></div>
+        <div className={`booking-phase phase-date ${phase == 3 && 'current'}`}>
             <span className="phase-number">
-                1
+                3
             </span>
             <span className="phase-name">
-                Date
+                Review
+            </span>
+        </div>
+        <div className="dot"></div>
+        <div className={`booking-phase phase-date ${phase == 4 && 'current'}`}>
+            <span className="phase-number">
+                4
+            </span>
+            <span className="phase-name">
+                Confirmation
             </span>
         </div>
     </div>
